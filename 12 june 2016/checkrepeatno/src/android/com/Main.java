db2dabd8934940d09bfc79edf6a9d269ffd9798c
Samra <samra_nisarsid27@outlook.com>
2016-06-23 13:14:06 +0500

    update 12 june checkrepeatno file/code

--- 12 june 2016/checkrepeatno/src/android/com/Main.java
+++ 12 june 2016/checkrepeatno/src/android/com/Main.java
@@ -11,14 +11,30 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
         Random a = new Random();
-        int i, j;
+        int integer, count;
+        int arr[] = new int[5];
+        for (int i = 0; i < arr.length; i++) {
+            do {
+                arr[i] = a.nextInt(10);
+                count = 0;//for checking previous array elements
+                while (arr[i] != arr[count]) {
+                    count++;
+
+                }
+            }
+            while (i != count);
+            System.out.println(arr[i]);
+        }
+    }
+}
+       /* int i, j;
         int arr[] = new int[5];
         for (i = 0; i < arr.length; i++) {
             arr[i] = a.nextInt(10);
             System.out.println("arr[" + i + "] = " + arr[i]);
         }
         for (j = 0; j < arr.length; j++) {
-            for (int k = j + 1; k < arr.length; k++) {
+            for (int k = j+1; k < arr.length; k++) {
                 if (arr[j] == arr[k] )
                 {
                     arr[k] = a.nextInt(10);
@@ -33,50 +49,4 @@ public class Main {
         for(int m=0;m<arr.length;m++)
         {
             System.out.println("arr["+m+"] = "+arr[m]);
-        }
-        /*for(i=0;i<arr.length;i++) {
-
-            if (i == 0) {
-                arr[i] = a.nextInt(10);
-            } else {
-                for (j = 0; j < arr.length; j++)
-                    if (n > i || n < 0) {
-                        break;
-                    }
-                if (arr[i] == arr[n-i]) {
-                    arr[i] = a.nextInt(10);
-                    if (arr[i] != arr[i - n]) {
-                        n++;
-                    } else {
-                        arr[i] = a.nextInt(10);
-                    }
-                }
-            }
         }*/
-
-            /*if (i == 0)
-            {
-                arr[i] = a.nextInt(10);
-            }
-            else {
-                arr[i] = a.nextInt(10);
-                inner:
-                for (int j = i - 1; j < arr.length; j--) {
-                    if (j > i|| j<0)
-                    {
-                        break inner;
-                    }
-                    else if (arr[i] == arr[j])
-                    {
-                        arr[i] = a.nextInt(10);
-                    }
-
-                }
-            }
-        }
-        for(int k=0;k<arr.length;k++)
-            System.out.println("arr["+k+"] = "+arr[k]);*/
-
-    }
-
-    }
