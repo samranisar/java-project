7d797c348bea09a17aa336274856970967d7fec4
Samra <samra_nisarsid27@outlook.com>
2016-07-15 16:02:30 +0500

    remove loop from numinwords code

--- 26june 2016/numinwords/src/android/com/Main.java
+++ 26june 2016/numinwords/src/android/com/Main.java
@@ -12,33 +12,25 @@ public class Main {
         Scanner x =new Scanner(System.in);
         System.out.println("Enter any no. from 1-100");
         int y = x.nextInt();
-        for(int i=0;i<=99;i++)
+        if(y>=1 && y<=9)
         {
-            if(y>=1 && y<=9)
-            {
-                System.out.println("In words:"+a[y-1]);
-                break;
-            }
-            else if(y>=11 && y<=19)
-            {
-                System.out.println("In words:"+b[y-11]);
-                break;
-            }
-            else if(y>=10 && y<=100 && y%10==0)
-            {
-                System.out.println("In words:"+c[(y/10)-1]);
-                break;
-            }
-            else if(y>=21 && y<=99 && y%10!=0)
-            {
-                System.out.println("In words:"+c[(y/10)-1]+" "+a[(y%10)-1]);
-                break;
-            }
-            else
-            {
-                continue;
-            }
+            System.out.println("In words:"+a[y-1]);
         }
-
+        else if(y>=11 && y<=19)
+        {
+            System.out.println("In words:"+b[y-11]);
+        }
+        else if(y>=10 && y<=100 && y%10==0)
+        {
+            System.out.println("In words:"+c[(y/10)-1]);
+        }
+        else if(y>=21 && y<=99 && y%10!=0)
+        {
+            System.out.println("In words:"+c[(y/10)-1]+" "+a[(y%10)-1]);
+        }
+        else
+        {
+            System.out.println("Entered wrong number");
         }
     }
+}
