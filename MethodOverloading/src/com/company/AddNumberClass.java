56200b5f9c4de7f69ee39c3bbfb201f4c4d75fe4
Samra <samra_nisarsid27@outlook.com>
2016-09-20 14:28:20 +0500

    18 sep methodOverloading task

---
+++ MethodOverloading/src/com/company/AddNumberClass.java
@@ -0,0 +1,23 @@
+package com.company;
+
+/**
+ * Created by lab2 on 18-Sep-16.
+ */
+public class AddNumberClass {
+
+    public float addNumbers(int a,float b){
+       return a+b;
+    }
+    public float addNumbers(float c,int d){
+        return c+d;
+    }
+
+     public int addNumbers(int e,int f){
+         return (e+f);
+     }
+    int mymethod(int a, int b, float c){
+        return(a+b);
+    }
+    //int  mymethod(int var1, int var2, float var3){}
+
+}
---
+++ MethodOverloading/src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+      /*  AddNumberClass first=new AddNumberClass();
+        System.out.println(first.addNumbers(5,6.5f));
+        System.out.println(first.addNumbers(6.5f,7));
+        System.out.println(first.addNumbers(4,6));
+        System.out.println();
+
+        AddNumberClass addNumber = new AddNumberClass();
+        System.out.println(addNumber. addNumbers(5,5));
+        System.out.println(addNumber. addNumbers(5,5.0f));
+        System.out.println(addNumber. addNumbers(5.0f,5));*/
+
+
+
+
+    }
+}
